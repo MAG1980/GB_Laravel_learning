f24ffc6026eb096d447e86d44a437d1ef7c96c8e
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2022-10-20 17:28:39 +0300

    Выполнено ДЗ к уроку №1. 1. Настроить на локальной машине окружение для работы с фреймворком. 2. Установить Laravel. 3. Реализовать несколько страниц будущего агрегатора новостей: a. Страницу приветствия пользователей. b. Страницу с информацией о проекте. c. Страницу для вывода одной и нескольких новостей.

---
+++ resources/views/main-menu.php
@@ -0,0 +1,6 @@
+<ul class="main-menu">
+    <li class="main-menu__li"><a class="main-menu__link" href="/">Страница приветствия</a></li>
+    <li class="main-menu__li"><a class="main-menu__link" href="/news">Список новостей</a></li>
+    <li class="main-menu__li"><a class="main-menu__link" href="/post">Новость</a></li>
+    <li class="main-menu__li"><a class="main-menu__link" href="/info">Информация о проекте</a></li>
+</ul>
