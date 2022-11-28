3bb642cebb8c9aef81ff2ef890adf2a2556c104b
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2022-11-28 15:57:52 +0300

    Добавлены тесты для форм добавления и удаления новостей.

--- tests/Browser/AdminNewsTest.php
+++ tests/Browser/AdminNewsTest.php
@@ -2,23 +2,22 @@
 
 namespace Tests\Browser;
 
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
+
 //Чтобы перед тестами в новую базу данных выполнялись миграции
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class AdminNewsTest extends DuskTestCase
 {
-    use DatabaseMigrations;
+
     //Чтобы перед тестами в новую базу данных выполнялись миграции
-    use RefreshDatabase;
+//    use RefreshDatabase;
     /**
      * A Dusk test example.
      *
      * @return void
      */
-    public function testTestFormAddNews()
+    public function testFormAddNewsFailValidate()
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/admin/news/create')
@@ -28,6 +27,31 @@ class AdminNewsTest extends DuskTestCase
                 ->press('Добавить новость')
                 //увидеть текст ошибки валидации
                 ->assertSee('Количество символов в поле наименование должно быть не меньше 3');
+        });
+    }
+
+    public function testFormAddNewsSuccessValidate()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/admin/news/create')
+                //набрать текст в поле формы
+                ->type('title', 111)
+                ->select('category_id', 1)
+                ->type('text', 'Текст новости')
+                //нажать на кнопку Добавить новость
+                ->press('Добавить новость')
+                //увидеть текст успешной валидации
+                ->assertSee('Новость успешно добавлена!');
+        });
+    }
+
+    public function testFormDeleteNewsSuccessValidate()
+    {
+        $this->browse(function (Browser $browser) {
+            //Нажать на кнопку и дождаться перезагрузки страницы
+            $browser->visit('/admin/news')
+                ->press('Удалить новость')
+                ->assertSee('Новость удалена успешно!');
         });
     }
 }
